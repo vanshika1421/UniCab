181dd60ba6f23aeee6e869460f9ee766920b9424
Vanshika <vanshikachhabra1407@gmail.com>
2025-09-26 10:39:09 +0530

    removed redi for now

--- config/default.js
+++ config/default.js
@@ -2,5 +2,4 @@
 module.exports = {
   jwtSecret: process.env.JWT_SECRET || 'fallback_secret_key_for_development',
   mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/unicab',
-  redisUrl: process.env.REDIS_URL || 'redis://localhost:6379',
 };
