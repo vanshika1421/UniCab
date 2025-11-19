b45c95f83766b6f7ca692831b5feb00c2761eb76
Vanshika <vanshikachhabra1407@gmail.com>
2025-11-19 20:11:38 +0530

    fixed feedback form

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ mongoose.connect(config.mongoURI)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+// Serve static assets from /public (CSS, client JS, images)
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
