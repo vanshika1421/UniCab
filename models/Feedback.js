4db273fa8515ea918dcc39adfa337e32c32e1f3d
Navya2065 <navyabhatia2065@gmail.com>
2025-11-19 18:52:40 +0530

    feedback feature added

---
+++ models/Feedback.js
@@ -0,0 +1,13 @@
+// models/Feedback.js
+const mongoose = require('mongoose');
+
+const feedbackSchema = new mongoose.Schema({
+  bookingId: { type: mongoose.Schema.Types.ObjectId, ref: 'Booking', required: true },
+  driverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  riderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  rating: { type: Number, min: 1, max: 5, required: true },
+  comment: { type: String },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Feedback', feedbackSchema);
